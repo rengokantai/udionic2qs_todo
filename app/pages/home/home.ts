159e2448a7bafefb7d98e1c12e0d627d81e11845
Hernan Y.Ke <yuriqiao@gmail.com>
2016-02-02 09:48:58 -0500

    todo component

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,16 @@
-import {Page} from 'ionic-framework/ionic';
+import {Page, NavController} from 'ionic-framework/ionic';
+import {TodoPage} from '../todo-page/todo-page';
+import {NavController} from "../../../node_modules/ionic-framework/components/nav/nav-controller";
 
 @Page({
   templateUrl: 'build/pages/home/home.html',
 })
 export class HomePage {
-  constructor() {
-
+  nav;
+  constructor(nav:NavController) {
+  this.nav=nav;
+  }
+  goTodo() {
+    this.nav.push(TodoPage);
   }
 }
---
+++ app/pages/todo-page/todo-page.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Hernan Y.Ke on 2016/2/2.
+ */
+import {Page} from 'ionic-framework/ionic';
+@Page({
+    templateUrl:'build/pages/todo-page/todo-page.html'
+})
+
+export class TodoPage{
+    constructor(){
+
+    }
+
+}
